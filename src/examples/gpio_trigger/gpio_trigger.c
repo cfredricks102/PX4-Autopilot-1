e264c71240876e326792e9d7a17f042566bae1f5
cfredricks102 <130706081+cfredricks102@users.noreply.github.com>
2023-04-13 14:18:33 -0700

    Add files via upload

---
+++ src/examples/gpio_trigger/gpio_trigger.c
@@ -0,0 +1,20 @@
+
+#include <px4_platform_common/log.h>
+
+__EXPORT int gpio_trigger_main(int argc, char *argv[]);
+
+int gpio_trigger_start_main(int argc, char *argv[])
+{
+	PX4_INFO("TAKING A PICTURE");
+
+    // px4_arch_configgpio(AUX_OUT_5);
+    // px4_arch_configgpio(AUX_OUT_6);
+
+    // px4_arch_gpiowrite(AUX_OUT_5,1);
+    // px4_arch_gpiowrite(AUX_OUT_6,1);
+
+    // px4_arch_gpiowrite(AUX_OUT_5,0);
+    // px4_arch_gpiowrite(AUX_OUT_6,0);
+
+	return OK;
+}
